2846a80b91df84921ba8e5aeeda970f9e1a5e7bb
京平 羽田 <210495@jc-21.jp>
2023-03-07 12:38:02 +0900

    type別id

--- 新しいフォルダー/itemtest/ソース.cpp
+++ 新しいフォルダー/itemtest/ソース.cpp
@@ -8,7 +8,11 @@ class Item
         MATERIAL = 0,   //素材
         TOOL = 1,       //道具
         WEAPON = 2,     //武器
+        MAX,
     }ItemType;
+private:
+    //アイテムの種類
+    ItemType itemType_;
 
     typedef struct
     {
@@ -16,46 +20,48 @@ class Item
         int  id_;
         //アイテムの名前
         std::string itemName_;
-        //アイテムの種類
-        ItemType itemType_;
         //アイテムの個数
         int itemCount_;
 
     }ItemInfo;
 
-    ItemInfo item_;
+    ItemInfo item_[MAX];
 public:
     Item() {}
     ~Item(){}
 
     //idを取得
-    int GetId() { return item_.id_; }
+    int GetId() { return item_[itemType_].id_; }
     //アイテム名を取得
-    std::string GetItemName() { return item_.itemName_; }
-    //アイテムの種類を取得
-    ItemType GetItemType() { return item_.itemType_; }
+    std::string GetItemName() { return item_[itemType_].itemName_; }
 
     //所持数カウントアップ
     void CountUp(int value=1)
     {
-        item_.itemCount_ += value;
+        item_[itemType_].itemCount_ += value;
     }
 
     //所持数カウントダウン
     void CountDown(int value = 1)
     {
-        item_.itemCount_ -= value;
+        item_[itemType_].itemCount_ -= value;
     }
 
     void SetId(int value)
     {
-        item_.id_ = value;
+        item_[itemType_].id_ = value;
+    }
+    void SetItemType_(int value)
+    {
+        itemType_ = static_cast<ItemType>(value);
     }
 };
 
 int main() {
     Item* item = new Item();
+    item->SetItemType_(1);
     item->SetId(1);
+    item->SetItemType_(0);
     std::cout << item->GetId();
     return 0;
 }
